cd68d82d11b826f3a5eceb2fd8193f12d7f01642
emaphis <emaphis85@gmail.com>
2023-08-31 18:28:52 -0400

    Finish Chapter 4

--- Chapter04/Script1.fsx
+++ Chapter04/Script1.fsx
@@ -1,13 +1,11 @@
 ﻿// Chapter 4
 // Introducing Imperative Programming
 
-
 // Imperative Looping and Iterating
 
 // Simple for Loops
 
 open System.IO
-open System.Net
 
 let http (url : string) =
     let req = System.Net.WebRequest.Create(url)
@@ -356,6 +354,7 @@ capitals.TryGetValue("USA");;
 // Using Dictionaries with Compound Keys
 
 open System.Collections.Generic;;
+open Microsoft.FSharp.Collections;;
 
 let sparseMap = new Dictionary<(int * int), float>();;
 //val sparseMap: Dictionary<(int * int),float> = dict []
@@ -372,7 +371,7 @@ sparseMap.Keys;;
 // Exceptions and Controlling Them
 
 //let req1 = System.Net.WebRequest.Create("not a URL");;
-//ystem.UriFormatException: Invalid URI: The format of the URI could not be determined.
+//System.UriFormatException: Invalid URI: The format of the URI could not be determined.
 
 
 //In F#, exceptions are commonly raised using the F# failwith function:
@@ -406,7 +405,6 @@ try
 with
     :? System.InvalidOperationException -> printfn "caught!"
 //caught!
-
 
 
 open System.IO
@@ -506,7 +504,8 @@ seq {
     for line in File.ReadLines tmpFile do     
         let words = line.Split [|' '|]
         if words.Length > 3 && words[2] = "easy" then
-            yield line }
+            yield line
+}
 //val it: string seq = seq ["It is easy to read."]
 
 
@@ -564,4 +563,414 @@ isCapital "Paris";;
 isCapital "Manchester";;
 //val it: bool = false
 
+// intermal table wordTable is computed as soon as isCapital is applied.
 
+
+// not precomputed
+
+let isCapitalSlow word = isWord ["London"; "Paris"; "Warsaw"; "Tokyo"] word;;
+
+// is is inefficient because because isWord
+// is applied to both its first argument and its
+// second argument every time you use the function isCapitalSlow.
+
+
+// inappropriate data structure for the lookup
+let isWordSlow2 (words: string list) (word: string) =
+    List.exists (fun word2 -> word = word2) words
+
+let isCapitalSlow2 word = isWordSlow2  ["London"; "Paris"; "Warsaw"; "Tokyo"] word
+
+
+// 
+let isWordSlow3 (words: string list) (word: string) =
+    let wordTable = Set<string>(words)
+    wordTable.Contains(word)
+
+let isCapitalSlow3 word = isWordSlow3 ["London"; "Paris"; "Warsaw"; "Tokyo"] word
+
+
+// Safe because HashSet is revealed to the world
+open System.Collections.Generic
+
+let isWord1 (words: string list) =
+    let wordTable = HashSet<string>(words)
+    fun word -> wordTable.Contains word
+
+
+// Precomputation and Objects
+
+// Listing 4-1. Precomputing a eord table before creating an object
+
+open System
+
+type NameLookupService =
+    abstract Contains: string -> bool
+
+let buildSimpleNameLookup (words: string list) =
+    let wordTable = HashSet<_>(words)
+    {new NameLookupService with
+        member t.Contains w = wordTable.Contains w}
+
+//type NameLookupService =
+//  abstract Contains: string -> bool
+//val buildSimpleNameLookup: words: string list -> NameLookupService
+
+
+let capitalLookup = buildSimpleNameLookup ["London"; "Paris"; "Warsaw"; "Tokyo"];;
+//val capitalLookup: NameLookupService
+
+capitalLookup.Contains "Paris";;
+//val it: bool = true
+
+capitalLookup.Contains "Pittsburg";;
+//val it: bool = false
+
+
+// Memoizing Computations
+
+// From Chapter 2, but modified to use stop watch
+let time f =
+    let sw = System.Diagnostics.Stopwatch.StartNew()
+    let res = f()
+    let finish = sw.Stop()
+    (res, sw.Elapsed.TotalMilliseconds |> sprintf "%f ms")
+//val time: f: (unit -> 'a) -> 'a * string
+
+
+let rec fibSlow n = if n <= 2 then 1 else fibSlow (n - 1) + fibSlow (n - 2)
+//val fibSlow: n: int -> int
+
+// sorta fast
+time (fun () -> fibSlow 30);;
+time (fun () -> fibSlow 30);;
+time (fun () -> fibSlow 30);;
+
+//val it: int * string = (832040, "3.706700 ms")
+//val it: int * string = (832040, "3.341500 ms")
+//val it: int * string = (832040, "3.057200 ms")
+
+// slow...
+time (fun () -> fibSlow 45);;
+time (fun () -> fibSlow 45);;
+time (fun () -> fibSlow 45);;
+
+//val it: int * string = (1134903170, "3480.051600 ms")
+//val it: int * string = (1134903170, "3500.174400 ms")
+//val it: int * string = (1134903170, "3388.321100 ms")
+
+
+// using a lookup table is much faster...
+
+let fibFast1 =
+    let t = new System.Collections.Generic.Dictionary<int, int>()
+    let rec fibCached n =
+        if t.ContainsKey n then t[n]
+        elif n <= 2 then 1
+        else let res = fibCached (n - 1) + fibCached (n - 2)
+             t.Add (n, res)
+             res
+    fun n -> fibCached n
+//val fibFast1: (int -> int)
+
+// fast
+time(fun () -> fibFast1 30);;
+time(fun () -> fibFast1 30);;
+time(fun () -> fibFast1 30);;
+
+//val it: int * string = (832040, "0.081600 ms")
+//val it: int * string = (832040, "0.053200 ms")
+//val it: int * string = (832040, "0.054800 ms")
+
+// still fast
+time(fun () -> fibFast1 45);;
+time(fun () -> fibFast1 45);;
+time(fun () -> fibFast1 45);;
+
+//val it: int * string = (1134903170, "0.070300 ms")
+//val it: int * string = (1134903170, "0.055100 ms")
+//val it: int * string = (1134903170, "0.079700 ms")
+
+
+// Listing 4-2. A generic memoization function
+
+open System.Collections.Generic
+
+// warning FS0040: This and other recursive references to the object(s) being
+// defined will be checked for initialization-soundness at runtime through the
+// use of a delayed reference. This is because you are defining one or more
+// recursive objects, rather than recursive functions. This warning may be
+// suppressed by using '#nowarn "40"' or '--nowarn:40'.
+
+#nowarn "40"
+
+let memoize (f: 'T -> 'U) =
+    let t = new Dictionary<'T, 'U>(HashIdentity.Structural)
+    fun n ->
+        if t.ContainsKey n then t[n]
+        else 
+            let res = f n
+            t.Add(n, res)
+            res
+//val memoize: f: ('T -> 'U) -> ('T -> 'U) when 'T: equality
+
+let rec fibFast2 =
+    memoize (fun n -> if n <= 2 then 1 else fibFast2 (n - 1) + fibFast2 (n - 2))
+//val fibFast2: (int -> int)
+
+
+// Omit the extra argument from the application of memoize, because including it 
+// would lead to a fresh memoization table being allocated each time the
+// function fibNotFast was called
+
+let rec fibNotFast n =
+    memoize (fun n -> if n <= 2 then 1 else fibNotFast (n - 1) + fibNotFast (n - 2)) n
+//val fibNotFast: n: int -> int
+
+
+// Listing 4-3. A generic memoization service
+// define a new variation on memoize that returns a Table object that supports
+// both a lookup and a Discard method
+
+
+open System.Collections.Generic
+
+type Table<'T, 'U> =
+    abstract Item : 'T -> 'U with get
+    abstract Discard : unit -> unit
+
+let memoizedAndPermitDiscard f =
+    let lookasideTable = new Dictionary<_,_>(HashIdentity.Structural)
+
+    {new Table<'T, 'U> with
+        member _t.Item
+            with get(n) =
+                if lookasideTable.ContainsKey(n) then
+                    lookasideTable[n]
+                else
+                    let res = f n
+                    lookasideTable.Add(n, res)
+                    res
+
+        member _t.Discard() =
+            lookasideTable.Clear() }
+
+//type Table<'T,'U> =
+//  abstract Discard: unit -> unit
+//  abstract Item: 'T -> 'U with get
+//val memoizedAndPermitDiscard: f: ('T -> 'U) -> Table<'T,'U> when 'T: equality
+
+
+#nowarn "40"
+
+let rec fibFast3 =
+    memoizedAndPermitDiscard (fun n ->
+    if n <= 2 then 1
+    else fibFast3[n - 1] + fibFast3[n - 2])
+//val fibFast3: Table<int,int>
+
+fibFast3[3];;
+//val it: int = 2
+
+fibFast3[5];;
+//val it: int = 5
+
+fibFast3[45];;
+//val it: int = 1134903170
+
+fibFast3.Discard();;
+
+
+// Lazy Values
+
+let sixty = lazy (30 + 30);;
+//val sixty: Lazy<int> = Value is not created.
+
+sixty.Force();;
+//val it: int = 60
+
+
+// Lazy Values with side effects
+
+let sixtyWithSideEffect = lazy (printfn "Hello world"; 30+30);;
+//val sixtyWithSideEffect: Lazy<int> = Value is not created.
+
+
+sixtyWithSideEffect.Force();;
+//Hello world
+//val it: int = 60
+
+
+sixtyWithSideEffect.Force();;
+//val it: int = 60
+
+
+// Mutable Reference Cells
+
+let cell4 = ref 1;;
+//val cell4: int ref = { contents = 1 }
+
+cell4.Value;;
+//val it: int = 1
+
+cell4.Value <- 3;;
+
+cell4;;
+//val it: int ref = { contents = 3 }
+
+cell4.Value;;
+//val it: int = 3
+
+
+//ref
+//val it: ('a -> 'a ref)
+
+//(:=)  -- deprecated:  Change 'cell := expr' to 'cell.Value <- expr'.
+//val it: ('a ref -> 'a -> unit)
+
+//(!) -- deprecated:  Change '!cell' to 'cell.Value'.
+//val it: ('a ref -> 'a)
+
+
+// some definitions
+
+//type 'T ref1 =
+//    {mutable contents : 'T}
+//    member cell.Value = cell.contents
+
+//let (!) r = r.contents
+
+//let ref1 v = {contents = v}
+
+//type 'T ref =
+//  { mutable contents: 'T }
+//  member Value: 'T
+//val (!) : r: 'a ref -> 'a
+//val ref: v: 'a -> 'a ref
+
+
+// Combining Functional and Imperative: Functional 
+// Programming with Side Effects
+
+// Consider Replacing Mutable Locals and Loops with Recursion
+
+let factorizeImperative n =
+    let mutable factor1 = 1
+    let mutable factor2 = n
+    let mutable i = 2
+    let mutable fin = false
+
+    while (i < n && not fin) do
+        if (n % i = 0) then
+            factor1 <- i
+            factor2 <- n / i
+            fin <- true
+        i <- i + 1
+
+    if (factor1 = 1) then None
+    else Some (factor1, factor2)
+//val factorizeImperative: n: int -> (int * int) option
+
+// inner recursive function:
+
+let factorizeRecursive n =
+    let rec find i =
+        if i >= n then None
+        elif (n % i = 0) then Some (i, n / i)
+        else find (i + 1)
+    find 2
+// val factorizeRecursive: n: int -> (int * int) option
+
+
+// Separating Pure Computation from Side-Effecting Computations
+
+// Separating Mutable Data Structures
+
+// Example divides a sequence of inputs into equivalence classes
+
+open System.Collections.Generic
+
+/// Example divides a sequence of inputs into equivalence classes
+let divideIntoEquivalenceClasses keyf seq =
+    // The dictionary to hold the equivalence classes
+    let dict = new Dictionary<'key, ResizeArray<'T>>()
+
+    // Build the groupings
+    seq |> Seq.iter (fun v ->
+        let key = keyf v
+        let ok, prev = dict.TryGetValue(key)
+        if ok then prev.Add(v)
+        else
+            let prev = new ResizeArray<'T>()
+            dict[key] <- prev
+            prev.Add(v))
+
+    // Return the sequence-of-sequences. Don't reveal the
+    // internal collections: just reveal them as sequences
+    dict |> Seq.map (fun group -> group.Key, Seq.readonly group.Value)
+
+//val divideIntoEquivalenceClasses:
+//   keyf: ('T -> 'key) -> seq: 'T seq -> ('key * 'T seq) seq when 'key: equality
+
+
+divideIntoEquivalenceClasses (fun n -> n % 3) [0 .. 10];;
+//val it: (int * int seq) seq =
+//  seq [(0, seq [0; 3; 6; 9]); (1, seq [1; 4; 7; 10]); (2, seq [2; 5; 8])]
+
+
+// Not All Side Effects Are Equal
+
+// Avoid Combining Imperative Programming and Laziness
+//
+// But it's reasonable to read from a file lazily
+
+// Note: create a "test.txt" file in .~/AppData/local/temp/ directory ...
+
+open System.IO
+
+let reader1, reader2 =
+    let reader = new StreamReader(File.OpenRead("test.txt"))
+    let firstReader() = reader.ReadLine();
+    let secondReader() = reader.ReadLine();
+
+    // Note: we close the stream reader here!
+    // But we are returning function values that use the reader
+    // This is very bad!
+    reader.Close()
+    firstReader, secondReader
+
+//val reader2: (unit -> string)
+//val reader1: (unit -> string)
+
+// Note: stream reader is now closed! The next line will fail!
+//let firstLine = reader1()
+//let secondLine = reader2()
+//firstLine, secondLine
+
+//System.ObjectDisposedException: Cannot read from a closed TextReader.
+//at System.IO.__Error.ReaderClosed()
+
+
+// Corrected
+open System.IO
+
+let line1, line2 =
+    let reader = new StreamReader(File.OpenRead("test.txt"))
+    let firstLine = reader.ReadLine();
+    let secondLine = reader.ReadLine();
+    reader.Close()
+    firstLine, secondLine
+
+//val line2: string = "This is two lines."
+//val line1: string = "This is one line."
+
+
+let linesOfFile =
+    seq { use reader = new StreamReader(File.OpenRead("test.txt"))
+          while not reader.EndOfStream do
+            yield reader.ReadLine() }
+
+
+linesOfFile;;
+//val it: string seq =
+//  seq ["This is one line."; "This is two lines."; "This is three lines."]
